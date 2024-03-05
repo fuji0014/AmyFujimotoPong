f819d371098eaaa9bff718e1097078477eb9bab8
Eimi Fujimoto <fuji0014@algonquinlive.com>
2024-03-05 18:14:51 -0500

    HUD in progress

--- AmyFujimotoPong/Source/AmyFujimotoPong/Ball/Ball.cpp
+++ AmyFujimotoPong/Source/AmyFujimotoPong/Ball/Ball.cpp
@@ -44,6 +44,19 @@ ABall::ABall()
 void ABall::BeginPlay()
 {	
 	Super::BeginPlay();
+
+	APlayerController* PC = GetWorld()->GetFirstPlayerController();
+	DisableInput(PC);
+
+	// Initialize the game timer
+	CurrentTime = GameStartsIn;
+
+	// Set a one-time timer to start the game after GameStartsIn seconds
+	GetWorld()->GetTimerManager().SetTimer(StartGameTimerHandle, this, &ABall::StartGameTimer, GameStartsIn + 1, false);
+
+	// Set a repeating timer for the countdown
+	GetWorld()->GetTimerManager().SetTimer(GameCountDownTimer, this, &ABall::CountDownTimer, 1, true);
+
 	SetActorRotation(SpawnDirection->GetRelativeRotation());
 	FVector ArrowDirection = GetActorForwardVector();
 	ProjectileMovement->Velocity = ArrowDirection * ProjectileMovement->InitialSpeed;
@@ -57,6 +70,30 @@ void ABall::BeginPlay()
 void ABall::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+}
+
+void ABall::StartGameTimer()
+{
+	//ABallGameState* GS = Cast<ABallGameState>(GetWorld()->GetGameState());
+	//GS->GameStarted = true;
+
+	APlayerController* PC = GetWorld()->GetFirstPlayerController();
+	EnableInput(PC);
+}
+
+void ABall::CountDownTimer()
+{
+	if (CurrentTime == 0)
+	{
+		GetWorld()->GetTimerManager().ClearTimer(GameCountDownTimer);
+		PrintMessageOnScreen(FString("Start Game"));
+		CurrentTime = 5;
+	}
+	else
+	{
+		CurrentTime--;
+		PrintMessageOnScreen(FString("Game Starts In: " + FString::FromInt(CurrentTime)));
+	}
 }
 
 void ABall::DefaultProjectileMovement()
---
+++ AmyFujimotoPong/Source/AmyFujimotoPong/PongGameState.cpp
@@ -0,0 +1,9 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PongGameState.h"
+
+void APongGameState::BeginPlay()
+{
+	Super::BeginPlay();
+}
